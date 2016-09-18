69f57fd99306d024a527f09dd9aaec76148e2a99
Trafium <trafium@gmail.com>
2016-09-18 21:22:37 +0300

    Bug Fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,7 @@ SampleApp::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
@@ -28,6 +28,7 @@ SampleApp::Application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
+  # config.assets.precompile = %w( *.css )
 
   # Generate digests for assets URLs.
   config.assets.digest = true
@@ -74,5 +75,5 @@ SampleApp::Application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   # Do not dump schema after migrations.
-  config.active_record.dump_schema_after_migration = false
+  # config.active_record.dump_schema_after_migration = false
 end
