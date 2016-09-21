e1d1687426a3fcc5873d1f894ec25de89651a742
DmitriAfanasjev <trafium@gmail.com>
2016-09-21 17:43:27 +0300

    Start doing followers/following

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,10 +10,26 @@ class User < ActiveRecord::Base
   validates :password, length: { minimum: 6 }
 
   has_many :microposts, dependent: :destroy
+  has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+  has_many :reverse_relationships, foreign_key: "followed_id", class_name: "Relationship", dependent: :destroy
+  has_many :followed_users, through: :relationships, source: :followed
+  has_many :followers, through: :reverse_relationships, source: :follower
 
   def feed
     # TEMP
     Micropost.where("user_id = ?", id)
+  end
+
+  def following?(other_user)
+    relationships.find_by(followed_id: other_user.id)
+  end
+
+  def follow!(other_user)
+    relationships.create!(followed_id: other_user.id)
+  end
+
+  def unfollow!(other_user)
+    relationships.find_by(followed_id: other_user.id).destroy!
   end
 
   def self.encrypt(token)
