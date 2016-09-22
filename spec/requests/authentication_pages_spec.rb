5e768615d7a3644748f43ee1e941c544db1e763e
DmitriAfanasjev <trafium@gmail.com>
2016-09-22 14:47:01 +0300

    Add user following

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -76,6 +76,16 @@ describe "AuthenticationPages" do
           before { visit users_path }
           it { should have_title('Sign in') }
         end
+
+        describe "visiting the following page" do
+          before { visit following_user_path(user) }
+          it { should have_title('Sign in') }
+        end
+
+        describe "visiting the followers page" do
+          before { visit followers_user_path(user) }
+          it { should have_title('Sign in') }
+        end
       end
 
       describe "as wrong user" do
@@ -103,7 +113,19 @@ describe "AuthenticationPages" do
         end
 
         describe "submitting to the destroy action" do
-          before { delete micropost_path(FactoryGirl.create(:micropost))}
+          before { delete micropost_path(FactoryGirl.create(:micropost)) }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+      end
+
+      describe "in the Relationships controller" do
+        describe "submitting to the create action" do
+          before { post relationships_path }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+
+        describe "submitting to the destroy action" do
+          before { delete relationship_path(1) }
           specify { expect(response).to redirect_to(signin_path) }
         end
       end
@@ -111,13 +133,13 @@ describe "AuthenticationPages" do
 
     describe "as non-admin user" do
       let(:user) { FactoryGirl.create(:user) }
-      let(:non_admin) {FactoryGirl.create(:user)}
+      let(:non_admin) { FactoryGirl.create(:user) }
 
-      before {sign_in non_admin, no_capybara: true}
+      before { sign_in non_admin, no_capybara: true }
 
       describe "submitting a DELETE request to the Users#destroy action" do
         before { delete user_path(user) }
-        specify { expect(response).to redirect_to(root_url)}
+        specify { expect(response).to redirect_to(root_url) }
       end
     end
   end
